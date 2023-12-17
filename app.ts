94716ca205835e501746259f36745d4a39160b4a
rishabsingla5@gmail.com <rishabsingla5@gmail.com>
2023-12-17 19:38:09 +0530

    finalCommit

---
+++ app.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import bodyParser from "body-parser";
+import router from "./routes/todo";
+const app = express();
+app.use(bodyParser.json());
+app.use(router);
+
+app.listen(3000);
